237603db40678ce9e8fc320f2594e2d3948dd294
sherifnabeeh <sherif.nabeeh@mail.utoronto.ca>
2020-11-05 22:45:57 +0200

    sherif

---
+++ phase1/src/domain/entities/Message.java
@@ -0,0 +1,58 @@
+package domain.entities;
+
+public class Message {
+
+    private String userId;
+    private String message;
+
+    /**
+     * A userId and message is required to create an instance of Message.
+     * @param userId
+     * @param message
+     */
+
+    public Event(String userId, String message){
+        this.userId = userId;
+        this.message = message;
+    }
+
+    /**
+     * Returns the User ID of the user who sent the message.
+     * @return userId
+     */
+
+    public String getUserId() {
+
+        return this.UserId;
+    }
+
+
+    /**
+     * This method sets the User ID of the user who sent the message.
+     * @param userId
+     */
+
+    public void setUserId(String ID) {
+
+        this.userId=ID;
+    }
+
+
+    /**
+     * Returns the message.
+     * @return message
+     */
+
+    public int getEventTime() {
+        return this.message;
+    }
+
+    /**
+     * This method sets the message.
+     * @param message
+     */
+
+    public void setMessage(String message) {
+        this.message=message;
+    }
+}
