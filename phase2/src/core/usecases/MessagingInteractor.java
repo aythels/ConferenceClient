69e6ba2db340862d3f0af46c3df5c173507fe08c
Felix Zhou <felix.rn.zhou@gmail.com>
2020-11-26 06:28:17 -0500

    Add attributes and constructor for MessagingInteractor

--- phase2/src/core/usecases/MessagingInteractor.java
+++ phase2/src/core/usecases/MessagingInteractor.java
@@ -1,6 +1,19 @@
 package core.usecases;
 
+import core.usecases.ports.IEventRepository;
+import core.usecases.ports.IMessageRepository;
+import core.usecases.ports.IUserRepository;
+
 public class MessagingInteractor {
 
+    private IMessageRepository messages;
+    private IUserRepository users;
+    private IEventRepository events;
+
+    public MessagingInteractor(IMessageRepository messages, IUserRepository users, IEventRepository events) {
+        this.messages = messages;
+        this.users = users;
+        this.events = events;
+    }
 
 }
