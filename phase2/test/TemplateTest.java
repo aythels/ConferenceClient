8fa8cc0a2fffbc86aaeb3efc625ea622002d8a88
Felix Zhou <felix.rn.zhou@gmail.com>
2020-11-26 05:33:11 -0500

    Update TemplateTest test class

--- phase2/test/TemplateTest.java
+++ phase2/test/TemplateTest.java
@@ -1,2 +1,15 @@
+import org.junit.*;
+import core.entities.Event;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class TemplateTest {
+
+    @Test
+    public void testEventDuration() {
+        Event e = new Event("test", LocalTime.of(1, 0), LocalTime.of(2, 0));
+        Duration duration = Duration.ofHours(1);
+        Assert.assertEquals(duration, e.getDuration());
+    }
 }
