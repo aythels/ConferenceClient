1d32d036870edc65976d2e2a51ddf7a9be560fd2
Felix Zhou <felix.rn.zhou@gmail.com>
2020-11-27 05:55:52 -0500

    Add LoginRepositoryTest

---
+++ phase2/test/adapters/LoginRepositoryTest.java
@@ -0,0 +1,44 @@
+package adapters;
+
+import org.junit.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LoginRepositoryTest {
+
+    private String store = "./store";
+    private String storeFile = "./store/logins.ser";
+
+    @Test
+    public void testPut() {
+        try {
+            File file = new File(storeFile);
+            file.delete();
+            {
+                LoginRepository repo = new LoginRepository(store);
+                repo.addLogin("test", "123");
+            }
+            LoginRepository repo = new LoginRepository(store);
+            Assert.assertTrue(repo.contains("test"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGet() {
+        try {
+            File file = new File(storeFile);
+            file.delete();
+            {
+                LoginRepository repo = new LoginRepository(store);
+                repo.addLogin("test", "123");
+            }
+            LoginRepository repo = new LoginRepository(store);
+            Assert.assertEquals(repo.getPassword("test"), "123");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
