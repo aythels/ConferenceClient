5c41764a79a9ae2ac49a9b2790d964be6f90c31c
Felix Zhou <felix.rn.zhou@gmail.com>
2020-11-28 21:20:11 -0500

    Update tests

--- phase2/test/adapters/LoginRepositoryTest.java
+++ phase2/test/adapters/LoginRepositoryTest.java
@@ -10,11 +10,15 @@ public class LoginRepositoryTest {
     private String store = "./teststore";
     private String storeFile = "./teststore/logins.ser";
 
+    @Before
+    public void deleteStore() {
+        File file = new File(storeFile);
+        file.delete();
+    }
+
     @Test
     public void testPut() {
         try {
-            File file = new File(storeFile);
-            file.delete();
             {
                 LoginRepository repo = new LoginRepository(store);
                 repo.addLogin("test", "123");
@@ -30,14 +34,30 @@ public class LoginRepositoryTest {
     @Test
     public void testGet() {
         try {
-            File file = new File(storeFile);
-            file.delete();
             {
                 LoginRepository repo = new LoginRepository(store);
                 repo.addLogin("test", "123");
             }
             LoginRepository repo = new LoginRepository(store);
             Assert.assertEquals(repo.getPassword("test"), "123");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testContains() {
+        try {
+            {
+               LoginRepository repo = new LoginRepository(store);
+               repo.addLogin("test", "password");
+                repo.addLogin("test1", "password");
+            }
+            LoginRepository repo = new LoginRepository(store);
+            Assert.assertTrue(repo.contains("test"));
+            Assert.assertTrue(repo.contains("test1"));
+            Assert.assertFalse(repo.contains("test2"));
         } catch (IOException e) {
             e.printStackTrace();
             Assert.fail();
