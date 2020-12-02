3dabb74964bc812247707fdcadea4d2eecde3189
Felix Zhou <felix.rn.zhou@gmail.com>
2020-12-01 21:18:10 -0500

    Change exception handling and add abstract constructor in AbstractRepository

--- phase2/src/gateways/AbstractRepository.java
+++ phase2/src/gateways/AbstractRepository.java
@@ -1,28 +1,38 @@
 package gateways;
 
 import java.io.*;
-import java.util.Optional;
+import java.nio.file.InvalidPathException;
 
 public class AbstractRepository {
 
-    public Optional<Object> get(File store) {
+    public AbstractRepository(String path) throws InvalidPathException {
+        File dir = new File(path);
+        if (!dir.isDirectory())
+            throw new InvalidPathException(path, "Given path is not a valid directory");
+    }
+
+    public Object get(File store) {
         try {
             FileInputStream fileIn = new FileInputStream(store);
             ObjectInputStream objIn = new ObjectInputStream(fileIn);
             Object obj = objIn.readObject();
             objIn.close();
             fileIn.close();
-            return Optional.of(obj);
-        } catch (ClassNotFoundException | IOException e) {
-            return Optional.empty();
+            return obj;
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void put(File store, Object data) throws IOException {
-        FileOutputStream fileOut = new FileOutputStream(store);
-        ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
-        objOut.writeObject(data);
-        objOut.close();
-        fileOut.close();
+    public void put(File store, Object data) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream(store);
+            ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+            objOut.writeObject(data);
+            objOut.close();
+            fileOut.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
