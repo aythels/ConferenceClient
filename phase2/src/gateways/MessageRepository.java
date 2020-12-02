373d9c54d7d71e8a52911cce1820c8562a552749
Felix Zhou <felix.rn.zhou@gmail.com>
2020-12-02 18:33:30 -0500

    Implement MessageRepository

--- phase2/src/gateways/MessageRepository.java
+++ phase2/src/gateways/MessageRepository.java
@@ -4,21 +4,35 @@ import core.usecases.Message;
 import core.usecases.ports.IMessageRepository;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class MessageRepository implements IMessageRepository {
+public class MessageRepository extends AbstractRepository implements IMessageRepository {
 
-    public MessageRepository(String path) {
+    private final File store;
+    private final List<Message> messages;
 
+    @SuppressWarnings("unchecked")
+    public MessageRepository(String path) throws IOException {
+        super(path);
+        store = new File(path, "messages");
+        if (store.createNewFile()) {
+            messages = new ArrayList<>();
+            put(store, messages);
+        } else {
+            messages = (List<Message>) get(store);
+        }
     }
 
     @Override
     public void storeMessage(Message message) {
-
+        messages.add(message);
+        put(store, message);
     }
 
     @Override
     public List<Message> getAllMessages() {
-        return null;
+        return messages;
     }
 }
